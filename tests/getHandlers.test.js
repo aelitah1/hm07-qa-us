b1a1623b47110e4c3d41e85eb791b9b3b55fd405
aelitah1 <aelitah1@yahoo.com>
2024-07-09 14:56:01 -0500

    Update getHandlers.test.js

--- tests/getHandlers.test.js
+++ tests/getHandlers.test.js
@@ -1,10 +1,26 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
-test('', async () => {
+test('Status should be 200....', async () => {
+	let actualStatusCode;
 	try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`);
+		const response = await fetch(`${config.API_URL}/api/v1/warehouses`);
+		actualStatusCode = response.status;
 	} catch (error) {
 		console.error(error);
-	}
+	} 
+	expect(actualStatusCode).toBe(200);
 });
+
+// eslint-disable-next-line no-undef
+test('Status should be 200....', async () => {
+	let actualResponse;
+	try {
+		const response = await fetch(`${config.API_URL}/api/v1/warehouses`);
+		actualResponse = await response.json();
+	} catch (error) {
+		console.error(error);
+	} 
+	console.log(actualResponse); 
+	expect(actualResponse[0]["name"]).toBe("Everything You Need")
+}); 
