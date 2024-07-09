d255bfd547983838696bbf8549a3dbeb431d1e4d
aelitah1 <aelitah1@yahoo.com>
2024-07-09 14:57:47 -0500

    Update putHandlers.test.js

--- tests/putHandlers.test.js
+++ tests/putHandlers.test.js
@@ -1,18 +1,52 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
-const requestBody = {}
+let requestBody = {
+    "productsList": [
+        {
+            "id": 1,
+            "quantity": 4
+        }
+        
+    ]
+}
 
-test('', async () => {
+test('should return 200...', async () => {
+    let actualstatus;
     try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`, {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/6`, {
 			method: 'PUT',
 			headers: {
 			'Content-Type': 'application/json'
 			},
 			body: JSON.stringify(requestBody)
 		});
-	} catch (error) {
-		console.error(error);
-	}
+        actualStatus = await response.status;
+       
+        expect(response.status).toBe(200);
+
+    } catch (error) {
+        console.error(error);
+    }
+    expect(actualStatus).toBe(200)
+   });
+
+
+
+test('should return ok true...', async () => {
+    let actualResponse;
+    try {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/6`, {
+			method: 'PUT',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(requestBody)
+		});
+        actualResponse = await response.json();
+        
+    } catch (error) {
+        console.error(error);
+    }
+    expect(actualResponse).toEqual("ok: true");
 });
